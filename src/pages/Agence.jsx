591de1435d9daf9b20192d9028da33ffe6e4aeb0
meetbhuva07 <meetbhuva0009@gmail.com>
2026-01-07 13:17:53 +0530

    today completed the agence page image animetion .

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -5,8 +5,21 @@ import React, { useRef } from 'react'
 
 const Agence = () => {
 
-  const imageDivRef = useRef(null)
   gsap.registerPlugin(ScrollTrigger)
+
+  const imageDivRef = useRef(null)
+  const imageRef = useRef(null)
+
+  const imageArray = [
+    'https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7',
+    'https://k72.ca/images/teamMembers/Olivier_480x640.jpg?w=480&h=640&fit=crop&s=c13569c0753117d04f1a93cf7b446d64',
+    'https://k72.ca/images/teamMembers/ChantalG_480x640.jpg?w=480&h=640&fit=crop&s=13093769c4a19cecd291ddcccd898991',
+    'https://k72.ca/images/teamMembers/Michele_480X640.jpg?w=480&h=640&fit=crop&s=ce85dc6d140947736baa739d0e59dab2',
+    'https://k72.ca/images/teamMembers/MEL_480X640.jpg?w=480&h=640&fit=crop&s=07c9bfee89816720b873e6748a276af6',
+    'https://k72.ca/images/teamMembers/CAMILLE_480X640_2.jpg?w=480&h=640&fit=crop&s=74317575b2d72fd11c5296615c383e4a',
+    'https://k72.ca/images/teamMembers/MEGGIE_480X640_2.jpg?w=480&h=640&fit=crop&s=3604b19f8fc7b40f517954147698d847',
+    'https://k72.ca/images/teamMembers/joel_480X640_3.jpg?w=480&h=640&fit=crop&s=1cadbf143b3aa916b1b414464acbb4d6',
+  ]
 
   useGSAP(function(){
     gsap.to(imageDivRef.current,{
@@ -14,9 +27,18 @@ const Agence = () => {
         trigger:imageDivRef.current,
         markers:true,
         start:'top 18.6%',
-        end:'top -100%',
-        scrub:true,
-        pin:true
+        end:'bottom -110%',
+        pin:true,
+        onUpdate:(elem) => {
+          let imageIndex;
+          if (elem.progress < 1) {
+            imageIndex = Math.floor(elem.progress * imageArray.length)
+          } else {
+            imageIndex = imageArray.length - 1
+          }
+
+          imageRef.current.src = imageArray[imageIndex]
+        }
       }
     })
   })
@@ -26,8 +48,8 @@ const Agence = () => {
 
       <div className='section1'>
       <div ref={imageDivRef} className=" absolute h-[20vw] w-[15vw] rounded-4xl  top-[8vw] left-[29.5vw] overflow-hidden">
-        <img className='h-full object-cover w-full' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="" />
-      </div>
+        <img ref={imageRef} className='h-full object-cover w-full' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="" />
+      </div>    
       <div className="font-[font2] relative">
       <div className="mt-[55vh]">
         <h1 className='text-[20vw] text-center uppercase leading-[18vw]'>SEVEN7Y <br />
